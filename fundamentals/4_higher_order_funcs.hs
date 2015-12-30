4bd1118eb7907a13b42939f5e6039fffd2c93bc0
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-12-30 11:34:02 -0800

    fix #1

--- fundamentals/4_higher_order_funcs.hs
+++ fundamentals/4_higher_order_funcs.hs
@@ -105,19 +105,20 @@ sumPair = map (\(a,b) -> a + b) [(1,2),(3,5),(6,3),(2,6),(2,5)]
 
 Reduce
 * Fold from left:
-    foldl <func acc cur> <acc> <list>,
-  where func has left acc
+    foldl <func :: acc -> cur -> a> <acc> <list>,
+  where func has acc on the LEFT
 * Fold from right
-    foldr <func <cur> <acc>> <acc> <list>,
-  where func has right acc
-* Only foldr workds on infinite list
+    foldr <func :: cur -> acc -> a> <acc> <list>,
+  where func has acc on the RIGHT
+* Only foldr rather than foldl works on infinite list
 
 Implicit starting value
 * foldl1 <func> <list>
 * foldr1 <func> <list>
 
 Scan
-* scanl, scanr, scanl1, scanr2
+* scanl, scanr, scanl1, scanr1
+scanl1 is a variant of scanl without starting value argument
 -}
 -- | usage of fold left, which starts from the left side.
 sumFold :: (Num a) => [a] -> a
@@ -130,6 +131,7 @@ elemFold :: (Eq a) => a -> [a] -> Bool
 elemFold y ys = foldl (\acc x -> if x == y then True else acc) False ys
 
 -- | when building new lists from a list, normally use foldr
+-- map using fold, accumulate to a list
 mapFoldl :: (a -> b) -> [a] -> [b]
 mapFoldl f xs = foldl (\acc x -> acc ++ [f x]) [] xs
 
@@ -144,11 +146,12 @@ headFold = foldr1 (\x _ -> x)  -- from right, take the cur
 lastFold :: [a] -> a
 lastFold = foldl1 (\_ x -> x)  -- from left, take the cur
 
+-- currying
 reverseFold :: [a] -> [a]
 reverseFold = foldl (\acc x -> x : acc) []
 
 -- scanl & scanr will display the intermediate result
--- thus more like dp
+-- thus like dp
 -- | laregest until now
 maxDP = scanl1 (\acc x -> if x > acc then x else acc) [3,4,5,3,7,9,2,1]
 
@@ -157,13 +160,15 @@ sqrtSums :: Int
 sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
 
 {-
-# Function Application $
+# Function Application `$`
 
-($) :: (a -> b) -> a -> b  -- infix
+($) :: (a -> b) -> a -> b  -- thus, without brackets, $ is infix op
 f $ x = f x
 
+* Normally left-associative
 * Function application with a space is LEFT-associative (so f a b c is the same
-as ((f a) b) c)), function application with $ is RIGHT-associative.
+as ((f a) b) c)), function application with $ is RIGHT-associative, since the
+left's precedence is reduced.
 * $ has the lowest precedence
 * Usage: 1) get rid of parentheseses 2) apply function
 
@@ -173,41 +178,58 @@ as ((f a) b) c)), function application with $ is RIGHT-associative.
 sumParenthesis = sum (map sqrt [1..130])
 sumFuncApply = sum $ map sqrt [1..130]
 
--- | pass function application as func
+-- | pass function application `$` as func
 funcApply = map ($ 3) [(4+), (10*), (^2), sqrt]
 -- ($ 3) is different from (($) 3) for param position.
 
 {-
-# Function Composition .
+# Function Composition `.`
 
+It is like mathematical funciton composition,
+and it is independent of function application `$`
 (.) :: (b -> c) -> (a -> b) -> a -> c
 f . g = \x -> f (g x)
 
-* Function composition is right associative
-* Only composes funcs that takes one param, thus currying is needed for
+* Function composition is RIGHT associative, rather than normal left associative
+* Only composes funcs that takes ONE param, thus currying is needed for
 multi-param func
 
 -}
 
 -- | Replace lambda expressions
-allNegLambda = map (\x -> negate (abs x)) [5,-3,-6,7,-3,2,-19,24]
-allNegComp = map (negate . abs) [5,-3,-6,7,-3,2,-19,24]
+negAllLambda = map (\x -> negate (abs x)) [5,-3,-6,7,-3,2,-19,24]
+negAllLambdaApplication = map (\x -> negate $ abs x) [5,-3,-6,7,-3,2,-19,24]
+negAllComp = map (negate . abs) [5,-3,-6,7,-3,2,-19,24]
 
 -- | Remove parentheseses
 funcParen = replicate 100 (product (map (*3) (zipWith max [1,2,3,4,5] [4,5,6,7,8])))
 funcComp = replicate 100 . product . map (*3) . zipWith max [1,2,3,4,5] $ [4,5,6,7,8]
+-- zipWith max [1,2,3,4,5] [4,5,6,7,8] works, but for the func composition above,
+-- it must have `$` to curry zipWith taking one param
 
 -- | Point free style (i.e. f(x) without param x)
 funcNormal x = ceiling (negate (tan (cos (max 50 x))))
 funcPointFree = ceiling . negate . tan . cos . max 50
 
+{-
+Function application vs. function composition
+k = a . b . c $ value
+is preferred over
+k = a $ b $ c $ value
+
+compose with (.) FIRST and then only at the END use ($) to append a value to
+evaluate the lot
+-}
+
 -- | Let binding vs. func composition
 oddSquareSum1 :: Integer
 oddSquareSum1 = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
 
+-- | this function style is preferred
 oddSquareSum2 :: Integer
 oddSquareSum2 = sum . takeWhile (<10000) . filter odd . map (^2) $ [1..]
 
+-- | for more readability
 oddSquareSum3 :: Integer
 oddSquareSum3 =
     let oddSquares = filter odd . map (^2) $ [1..]
